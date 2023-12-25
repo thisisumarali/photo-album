8361c51e5953e9cda3f5f473f621861ac7bb77e2
Umar Ali <umeralikhan7864@gmail.com>
2023-12-26 00:37:22 +0500

    'change'

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,18 +15,6 @@ export default function Home() {
   const [imageId, setImageId] = useState('cld-sample-4')
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-
-      <CldUploadButton uploadPreset="pinvtb9w" onUpload={(results) => {
-        let res = results as UploadImage
-        setImageId(res.info.public_id)
-      }} />
-      <CldImage
-        width="400"
-        height="400"
-        src={imageId}
-        sizes="100vw"
-        alt="Description of my image"
-      />
     </main>
   )
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,6 +5,7 @@ import {
 } from "@/components/ui/avatar"
 import { ModeToggle } from "./ui/ModeToggle"
 import MobileNavbar from "./MobileNavbar"
+import Link from "next/link"
 
 const Navbar = () => {
     return (
@@ -12,7 +13,9 @@ const Navbar = () => {
             <div className="border-b">
                 <div className="flex h-16 items-center px-4">
 
-                    <h1 className='text-3xl font-bold tracking-tight'>FotoFolio</h1>
+                    <Link href="/gallery">
+                        <h1 className='text-3xl cursor-pointer font-bold tracking-tight'>FotoFolio</h1>
+                    </Link>
                     <div className="ml-auto flex items-center space-x-4">
                         <div className="block lg:hidden">
                             <MobileNavbar />
